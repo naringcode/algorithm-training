3fb44303c1b3afd867d53822c42c65936d0cced5
naringcode <naringcode@gmail.com>
2023-03-10 07:07:42 +0900

    2023.03.10

---
+++ src/25314.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    cin >> n;
+
+    n /= 4;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "long ";
+    }
+
+    cout << "int";
+
+    return 0;
+}
+
