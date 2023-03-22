52c6f470391f4a37175f9d8e97804e9a7b4a9da4
naringcode <naringcode@gmail.com>
2023-03-22 23:39:08 +0900

    27866

---
+++ src/27866..cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char str[1004];
+int i;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> str >> i;
+
+    cout << str[i - 1];
+
+    return 0;
+}
