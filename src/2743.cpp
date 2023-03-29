d391303228f8d14af579a99c2b4625270d7f3f58
naringcode <naringcode@gmail.com>
2023-03-29 22:29:36 +0900

    2743

---
+++ src/2743.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char str[256];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> str;
+
+    cout << std::strlen(str);
+
+    return 0;
+}
