39e75f59c0d354cd2d602902963d0222ca34deef
naringcode <naringcode@gmail.com>
2023-04-06 07:51:25 +0900

    11718.cpp

---
+++ src/11718.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string str;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    while (getline(cin, str))
+    {
+        cout << str << '\n';
+    }
+    
+    return 0;
+}
