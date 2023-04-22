5921daf54843df3e37c18c4e19935220d153e2a3
naringcode <naringcode@gmail.com>
2023-04-23 06:35:56 +0900

    2742

---
+++ src/2742.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    while (n != 0)
+    {
+        cout << n << '\n';
+
+        n--;
+    }
+
+    return 0;
+}
