79e7b366d765e94b9367e7be966a35ce7261abd3
naringcode <naringcode@gmail.com>
2023-08-30 23:13:59 +0900

    10718

---
+++ src/easy/10718.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << "강한친구 대한육군\n강한친구 대한육군";
+
+    return 0;
+}
