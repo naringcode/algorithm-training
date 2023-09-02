5309aa98c8eb1c454178d787f1c7ab212d3c9f41
naringcode <naringcode@gmail.com>
2023-09-02 23:45:29 +0900

    5338

---
+++ src/easy/5338.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << "       _.-;;-._" << '\n';
+    cout << "'-..-'|   ||   |" << '\n';
+    cout << "'-..-'|_.-;;-._|" << '\n';
+    cout << "'-..-'|   ||   |" << '\n';
+    cout << "'-..-'|_.-''-._|" << '\n';
+
+    return 0;
+}
