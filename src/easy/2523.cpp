3f36f4abffd5bec8b911a4551667bdc2af8db9bb
naringcode <naringcode@gmail.com>
2023-09-10 23:12:15 +0900

    2523

---
+++ src/easy/2523.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << '*';
+        }
+
+        cout << '\n';
+    }
+    
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < (n - i) - 1; j++)
+        {
+            cout << '*';
+        }
+
+        cout << '\n';
+    }
+    
+    return 0;
+}
