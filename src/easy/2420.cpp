be8b4ef0313305dca432a270556340184fe3262e
naringcode <naringcode@gmail.com>
2023-09-22 22:13:04 +0900

    2420

---
+++ src/easy/2420.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long n;
+long long m;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> m;
+
+    cout << abs(n - m);
+
+    return 0;
+}
