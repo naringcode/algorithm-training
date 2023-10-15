87f38db7ff839b159ee165e2b4ba4d6f5604c8d5
naringcode <naringcode@gmail.com>
2023-10-15 22:15:35 +0900

    2163

---
+++ src/easy/2163.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int m;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> m;
+
+    cout << n * m - 1;
+
+    return 0;
+}
