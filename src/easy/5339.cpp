5769b9679f8b6ba19822cc3d7263a519d3b83d31
naringcode <naringcode@gmail.com>
2023-10-16 21:27:39 +0900

    5339

---
+++ src/easy/5339.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << "     /~\\" << '\n';
+    cout << "    ( oo|" << '\n';
+    cout << "    _\\=/_" << '\n';
+    cout << "   /  _  \\" << '\n';
+    cout << "  //|/.\\|\\\\" << '\n';
+    cout << " ||  \\ /  ||" << '\n';
+    cout << "============" << '\n';
+    cout << "|          |" << '\n';
+    cout << "|          |" << '\n';
+    cout << "|          |" << '\n';
+
+    return 0;
+}
