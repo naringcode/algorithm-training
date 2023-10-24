a049724d9a71776decff86a4f40d532eb53775f6
naringcode <naringcode@gmail.com>
2023-10-25 00:30:25 +0900

    23234

---
+++ src/easy/23234.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << "The world says hello!";
+
+    return 0;
+}
