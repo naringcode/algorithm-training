617260d2941eaa8af26773ee060542c3182e8c16
naringcode <naringcode@gmail.com>
2023-11-09 23:15:45 +0900

    2455

---
+++ src/easy/2455.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b;
+
+int curr;
+int mx;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> a >> b;
+
+        curr += (b - a);
+
+        mx = max(mx, curr);
+    }
+
+    cout << mx;
+
+    return 0;
+}
