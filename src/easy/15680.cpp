6adee1dc539f5c84c935d4fc477c243f4d7b4029
naringcode <naringcode@gmail.com>
2023-11-17 23:59:24 +0900

    15680

---
+++ src/easy/15680.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    if (0 == n)
+    {
+        cout << "YONSEI";
+    }
+    else
+    {
+        cout << "Leading the Way to the Future";
+    }
+
+    return 0;
+}
