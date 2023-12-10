db6ff1424b8a516f0914615d3fe4c54c077c5605
naringcode <naringcode@gmail.com>
2023-12-10 23:29:09 +0900

    5717

---
+++ src/easy/5717.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a;
+int b;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    while (true)
+    {
+        cin >> a >> b;
+
+        if (0 == a && 0 == b)
+            break;
+
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
