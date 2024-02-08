ab2ebc919738915608ac608bcbe50f7de932460f
naringcode <naringcode@gmail.com>
2024-02-08 22:08:16 +0900

    3046

---
+++ src/easy/3046.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int r1;
+int s;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> r1 >> s;
+
+    cout << 2 * s - r1;
+
+    return 0;
+}
