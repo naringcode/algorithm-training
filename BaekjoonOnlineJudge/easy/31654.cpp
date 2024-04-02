5805dd1d66778a32709e0a578ae2d1a584ecbcca
naringcode <naringcode@gmail.com>
2024-04-02 23:20:30 +0900

    31654

---
+++ BaekjoonOnlineJudge/easy/31654.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+ll a;
+ll b;
+ll c;
+
+int main()
+{
+    cin >> a >> b >> c;
+
+    if (c == a + b)
+    {
+        cout << "correct!";
+    }
+    else
+    {
+        cout << "wrong!";
+    }
+
+    return 0;
+}
