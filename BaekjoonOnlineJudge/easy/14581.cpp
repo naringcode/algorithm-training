cf5bd47bd8905a7e8afb25d4c0a3445f32f97a9d
naringcode <naringcode@gmail.com>
2024-06-24 22:21:15 +0900

    14581

---
+++ BaekjoonOnlineJudge/easy/14581.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string str;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> str;
+
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << str << "::fan:\n";
+    cout << ":fan::fan::fan:\n";
+
+    return 0;
+}
