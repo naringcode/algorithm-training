34ec4cc7c764c8d12eb9b70117b32c537d702dc6
naringcode <naringcode@gmail.com>
2024-08-30 23:25:20 +0900

    15700

---
+++ BaekjoonOnlineJudge/easy/15700.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long a;
+long long b;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> a >> b;
+
+    cout << a * b / 2;
+
+    return 0;
+}
