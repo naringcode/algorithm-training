42af0c8d636657814bc2330977f5f03319da0e03
naringcode <naringcode@gmail.com>
2025-01-03 22:54:21 +0900

    7287

---
+++ BaekjoonOnlineJudge/_basic/7287.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << 890 << '\n';
+    cout << "naringcode";
+
+    return 0;
+}
