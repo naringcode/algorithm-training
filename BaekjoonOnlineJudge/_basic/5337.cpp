d194ebaa4e1f7bf0db5a8d4da0095d8c6aacd3e9
naringcode <naringcode@gmail.com>
2025-01-07 23:00:19 +0900

    5337

---
+++ BaekjoonOnlineJudge/_basic/5337.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << R"(.  .   .)" << '\n'; 
+    cout << R"(|  | _ | _. _ ._ _  _)" << '\n'; 
+    cout << R"(|/\|(/.|(_.(_)[ | )(/.)"; 
+
+    return 0;
+}
