c0fc3d4c91054e259b482506f4515a37cfe893e3
naringcode <naringcode@gmail.com>
2025-01-11 22:58:28 +0900

    9316

---
+++ BaekjoonOnlineJudge/_basic/9316.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Hello World, Judge " << i << "!\n";
+    }
+
+    return 0;
+}
