bbb8ecbfd9567b4eccadfe88c71113ab4807f0cc
naringcode <naringcode@gmail.com>
2025-01-25 21:51:46 +0900

    16394

---
+++ BaekjoonOnlineJudge/_basic/16394.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    cout << n - 1946;
+
+    return 0;
+}
