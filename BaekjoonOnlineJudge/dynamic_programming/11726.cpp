976304f253914749b0bcc5143a9d046dff08ddda
naringcode <naringcode@gmail.com>
2025-02-16 22:44:44 +0900

    11726

---
+++ BaekjoonOnlineJudge/dynamic_programming/11726.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+ll arr[1004];
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // 1, 2, 3, 5, 8, ...
+    arr[1] = 1;
+    arr[2] = 2;
+
+    for (int i = 3; i <= 1000; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+        arr[i] %= 10007;
+    }
+
+    cin >> n;
+
+    cout << arr[n];
+
+    return 0;
+}
