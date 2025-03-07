e5c69cd95eed47314b599fe10f4b18e6625dcb60
naringcode <naringcode@gmail.com>
2025-03-07 22:36:03 +0900

    1402

---
+++ BaekjoonOnlineJudge/ad_hoc/1402.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <ranges>
+#include <print>
+
+using namespace std;
+
+using ll = long long;
+
+int t;
+int a;
+int b;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> a >> b;
+
+        cout << "yes\n";
+    }
+
+    return 0;
+}
