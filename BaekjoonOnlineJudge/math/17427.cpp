5b90ffc1477a5e266fce0a75c00a2ba907c4e03c
naringcode <naringcode@gmail.com>
2025-05-13 22:23:55 +0900

    17427

--- BaekjoonOnlineJudge/math/17427.cpp
+++ BaekjoonOnlineJudge/math/17427.cpp
@@ -1,1 +1,27 @@
-c:\Workspace\Inflearn\10w_Kundol_Algorithm\Board\Whiteboard08.cpp
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+ll n;
+ll res;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    cin >> n;
+
+    for (int num : views::iota(1, n + 1))
+    {
+        // 약수의 개수를 누적
+        res += num * (n / num);
+    }
+
+    println("{}", res);
+
+    return 0;
+}
