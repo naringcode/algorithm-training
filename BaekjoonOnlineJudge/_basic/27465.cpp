0d7741588bf0db78301ce228dad58ecb52f80ce1
naringcode <naringcode@gmail.com>
2025-07-24 23:18:17 +0900

    27465

---
+++ BaekjoonOnlineJudge/_basic/27465.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    println("1000000000");
+
+    return 0;
+}
