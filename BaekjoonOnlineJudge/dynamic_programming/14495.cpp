c25aa1cc6c42dd87fe10a0ae8cc1e1a86e1aa927
naringcode <naringcode@gmail.com>
2025-08-11 22:35:37 +0900

    14495

---
+++ BaekjoonOnlineJudge/dynamic_programming/14495.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int n;
+ll dp[120];
+
+ll fibo(int n)
+{
+    ll& memo = dp[n];
+    if (memo != -1)
+        return memo;
+
+    memo = fibo(n - 1) + fibo(n - 3);
+
+    return memo;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ranges::fill(dp, -1);
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
+    dp[4] = 2;
+
+    cin >> n;
+
+    println("{}", fibo(n));
+
+    return 0;
+}
