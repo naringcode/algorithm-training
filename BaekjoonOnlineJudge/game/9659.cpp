86e7e668f4867f56239f035ce528b158fc6aeca3
naringcode <naringcode@gmail.com>
2025-08-22 23:05:36 +0900

    9659

---
+++ BaekjoonOnlineJudge/game/9659.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+ll n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    if (n % 2)
+    {
+        println("SK");
+    }
+    else
+    {
+        println("CY");
+    }
+
+    return 0;
+}
