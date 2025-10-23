7971464e71ab7070535eca868f9c0f43820a2418
naringcode <naringcode@gmail.com>
2025-10-23 23:12:00 +0900

    6186

---
+++ BaekjoonOnlineJudge/graph/dfs/6186.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+const int dy[]{ -1, 0, 1, 0 };
+const int dx[]{ 0, 1, 0, -1 };
+
+int r;
+int c;
+
+char board[104][104];
+bool visited[104][104];
+
+int res;
+
+void go(int y, int x)
+{
+    if (visited[y][x] == true)
+        return;
+
+    visited[y][x] = true;
+
+    for (int dir : views::iota(0, ssize(dy)))
+    {
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+
+        if (ny < 0 || nx < 0 || ny >= r || nx >= c)
+            continue;
+
+        if (board[ny][nx] == '.')
+            continue;
+
+        go(ny, nx);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> r >> c;
+
+    for (auto [y, x] : views::cartesian_product(views::iota(0, r), views::iota(0, c)))
+    {
+        cin >> board[y][x];
+    }
+
+    for (auto [y, x] : views::cartesian_product(views::iota(0, r), views::iota(0, c)))
+    {
+        if (board[y][x] == '#' && visited[y][x] == false)
+        {
+            res++;
+
+            go(y, x);
+        }
+    }
+    
+    println("{}", res);
+
+    return 0;
+}
