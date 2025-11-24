bbc05683b90232e92e362ff6e54793a342ceac3f
naringcode <naringcode@gmail.com>
2025-11-24 22:50:56 +0900

    14935

---
+++ BaekjoonOnlineJudge/_basic/14935.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+ll x;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> x;
+
+    println("FA");
+
+    return 0;
+}
