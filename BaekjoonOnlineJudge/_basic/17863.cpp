68da3c6efe85f10a01891030fe7b8192fb3af71c
naringcode <naringcode@gmail.com>
2025-12-06 22:51:31 +0900

    17863

---
+++ BaekjoonOnlineJudge/_basic/17863.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string str;
+    cin >> str;
+
+    if (str[0] == '5' && str[1] == '5' && str[2] == '5')
+    {
+        println("YES");
+    }
+    else
+    {
+        println("NO");
+    }
+
+    return 0;
+}
