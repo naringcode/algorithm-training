5e7dca746303174d42bef7b8611d4f1afbcce7c5
naringcode <naringcode@gmail.com>
2026-02-27 17:08:17 +0900

    18301

---
+++ BaekjoonOnlineJudge/_basic/18301.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n1;
+int n2;
+int n3;
+
+int main()
+{
+    cin >> n1 >> n2 >> n3;
+    
+    cout << (n1 + 1) * (n2 + 1) / (n3 + 1) - 1;
+    
+    return 0;
+}
