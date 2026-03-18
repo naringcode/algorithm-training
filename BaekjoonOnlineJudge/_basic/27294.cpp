5ac9bcb7b418ff74b79451f89c8fc438cf5bba5b
naringcode <naringcode@gmail.com>
2026-03-18 18:51:43 +0900

    27294

---
+++ BaekjoonOnlineJudge/_basic/27294.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t;
+int s;
+
+int main()
+{
+	cin >> t >> s;
+	
+	if (t >= 12 && t <= 16 && s == 0)
+	{
+	    cout << 320;
+	}
+	else
+	{
+	    cout << 280;
+	}
+	
+	return 0;
+}
