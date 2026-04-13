ab2e6caa81dc6cc2c0ccc13ec8add437ba637b21
naringcode <naringcode@gmail.com>
2026-04-14 05:40:54 +0900

    23530

---
+++ BaekjoonOnlineJudge/_basic/23530.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int t;
+int a;
+int b;
+int c;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> a >> b;
+
+        println("1");
+    }
+
+    return 0;
+}
